1e5e672543f97328828a265783d117c3edca3b99
PhilJHazell2 <philiphazell@gmail.com>
2025-11-07 10:38:31 +0000

    DC feat: adds solution for dice game challenge

--- dice-game-challenge/challenge-code.js
+++ dice-game-challenge/challenge-code.js
@@ -1,3 +1,23 @@
-const rollDice = () => {}
+const rollDice = ( numberOfRolls ) => {
+	const results = [];
+	let dice_1;
+	let dice_2;
+	let total;
+	let result;
+
+	for( let i = 0; i < numberOfRolls; i++ ) {
+		dice_1 = generateIndividualDiceRoll();
+		dice_2 = generateIndividualDiceRoll();
+		total = dice_1 + dice_2;
+		result = dice_1 === dice_2 ? 'You win' : 'You lose';
+		results.push( { dice_1, dice_2, total, result } );
+	}
+
+	return results;
+}
+
+const generateIndividualDiceRoll = () => {
+		return Math.floor( Math.random() * 6 ) + 1;
+}
 
 module.exports = rollDice;
